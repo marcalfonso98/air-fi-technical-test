3e9a9ba89f9d2483f9415c1b8b5a3801c329b1d9
marcalfonso98 <malfonso621@gmail.com>
2025-06-11 21:30:41 +0200

    Console Email Working, Email Format Validation on Backend Working

--- backend/qr_scanner_webapp/scanner_api/views.py
+++ backend/qr_scanner_webapp/scanner_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status, permissions
 
 from .scan_serializer import ScanSerializer
+from django.core.mail import send_mail
 
 # Create your views here.
 class ScanQR(APIView):
@@ -20,7 +21,25 @@ class ScanQR(APIView):
         qr_data = serializer.validated_data['qr_content']
         user_email = serializer.validated_data['user_email']
         
-        # Get the validated user
+        # Get the username of the authenticated user
         user = request.user
+        
+        mail = send_mail(
+            "Resumen Escaneo QR",
+            f'''
+                Hola {user},\n\n
+                Te informamos que has realizado un escáner con éxito y 
+                a continuación tienes la información asociada:\n
+                Contenido del QR: {qr_data}
+                Nombre de usuario: {user}\n
+                Gracias,
+                Equipo de Escáneres Air-Fi
+            ''',
+            "noreply_scanner@gmail.com", # From
+            ["malfonso621@gmail.com"], # To
+            fail_silently=False
+        )
+        
+
         
         return Response({"message": "Scan API Works"})
